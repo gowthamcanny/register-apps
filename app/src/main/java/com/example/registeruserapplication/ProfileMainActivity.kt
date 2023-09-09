cb621ddaafcc247320ea371a6fff953afe46c4e6
Gowtham <gowthamrajofficial96@gmail.com>
2023-09-09 15:59:46 +0530

    ui correction

--- app/src/main/java/com/example/registeruserapplication/ProfileMainActivity.kt
+++ app/src/main/java/com/example/registeruserapplication/ProfileMainActivity.kt
@@ -2,6 +2,10 @@ package com.example.registeruserapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.example.registeruserapplication.databinding.ActivityMainBinding
 
@@ -10,6 +14,11 @@ class ProfileMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = ViewCompat.getWindowInsetsController(window.decorView)
+        controller?.hide(WindowInsetsCompat.Type.systemBars())
+
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
